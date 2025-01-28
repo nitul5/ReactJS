64a1acc573a512b38a6fbe5b0fcca38568ad21a2
Nitul Das <sziekcondearl123@gmail.com>
2025-01-28 13:33:34 +0530

    ProgrammingNavigate.jsx
    How to use useNavigate hook, for programmatic navigating...

--- ProgrammingNavigate.jsx
+++ ProgrammingNavigate.jsx
@@ -1,1 +1,12 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom'; // importing useNavigate hook
 
+export function OrderDone(){
+    const navigate = useNavigate(); // storing useNavigate() function in a variable as we know, it is a top level function
+
+    return(
+        <>
+        <button onClick={()=> navigate('/order-summary')}>Place-Order</button> //navigate('We have to specify in which page we want to navigate by clicking on that button')
+        </>
+    )
+}
